e91f47a867edbb453a2baa383f426080fcc98226
ayushbharadva25 <ayushbharadva25@gmail.com>
2024-03-16 19:27:56 +0530

    Pexels-Clone

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<p>Image-Gallery</p>
+		</>
+	);
+}
+
+export default App;
